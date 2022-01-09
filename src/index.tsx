44a1460cd306dfddc66d17c2ba9b58181a8dd908
Matthew Stanciu <mattbstanciu@gmail.com>
2022-01-09 12:52:54 -0500

    init

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import { Color, List, Icon, Detail, ImageMask, Form, ActionPanel, render } from "@raycast/api";
+import { useState } from "react";
+
+export default function main() {
+  const [text, setText] = useState<string>("33fd8f");
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <ActionPanel.Item
+            title="See Color"
+            onAction={() => {
+              render(
+                <List>
+                  <List.Item
+                    title={text.replace("#", "")}
+                    icon={{
+                      source: `https://singlecolorimage.com/get/${text.replace("#", "")}/100x100`,
+                      mask: ImageMask.Circle,
+                    }}
+                  />
+                </List>
+              );
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField id="color" value={text} defaultValue="33fd8f" onChange={setText} />
+      {/*      <List>
+        <List.Item title="Blue" icon={{ source: Icon.XmarkCircle, tintColor: "#FD667F" }} />
+        <List.Item
+          title="Icon"
+          icon={{ source: "https://singlecolorimage.com/get/33fd8f/100x100", mask: ImageMask.Circle }}
+        />
+      </List> */}
+    </Form>
+  );
+}
